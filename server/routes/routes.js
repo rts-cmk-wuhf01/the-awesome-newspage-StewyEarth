96f787f99ea87930f4dd5dc4bc5cbd27d6623f30
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-05-28 10:51:36 +0200

    Created routes, made header,nav and heoarea partial

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,5 +3,17 @@ module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home');
    });
+   app.get('/about', (req, res, next) => {
+      res.render('about');
+   });
+   app.get('/catagories-post', (req, res, next) => {
+      res.render('catagories-post');
+   });
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
+   });
+   app.get('/single-post', (req, res, next) => {
+      res.render('single-post');
+   });
 
 };
