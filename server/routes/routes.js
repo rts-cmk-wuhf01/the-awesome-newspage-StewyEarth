3aba0cc17ebba3c54ae6940abd3c6aaf9492ed2a
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-05-29 12:30:48 +0200

    Latest news lavet med foreach

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,44 @@
 module.exports = (app) => {
+let latestNewsData = [
+   {
+      imgPath: "img/bg-img/19.jpg",
+      postCategory: "Finance",
+      title: "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+      postTime: "7:00 AM",
+      postDate: "April 14"
+   },
+   {
+      imgPath: "img/bg-img/20.jpg",
+      postCategory: "Politics",
+      title: "Sed a elit euismod augue semper congue sit amet ac sapien.",
+      postTime: "7:00 AM",
+      postDate: "April 14"
+   },{
+      imgPath: "img/bg-img/21.jpg",
+      postCategory: "Health",
+      title: "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+      postTime: "7:00 AM",
+      postDate: "April 14"
+   },{
+      imgPath: "img/bg-img/22.jpg",
+      postCategory: "Finance",
+      title: "Augue semper congue sit amet ac sapien. Fusce consequat.",
+      postTime: "7:00 AM",
+      postDate: "April 14"
+   },{
+      imgPath: "img/bg-img/23.jpg",
+      postCategory: "Travel",
+      title: "Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.",
+      postTime: "7:00 AM",
+      postDate: "April 14"
+   },{
+      imgPath: "img/bg-img/24.jpg",
+      postCategory: "Politics",
+      title: "Augue semper congue sit amet ac sapien. Fusce consequat.",
+      postTime: "7:00 AM",
+      postDate: "April 14"
+   }
+]
 
    app.get('/', (req, res, next) => {
       res.render('home');
@@ -7,13 +47,17 @@ module.exports = (app) => {
       res.render('about');
    });
    app.get('/catagories-post', (req, res, next) => {
-      res.render('catagories-post');
+      res.render('catagories-post',{
+         latestNews: latestNewsData
+      });
    });
    app.get('/contact', (req, res, next) => {
       res.render('contact');
    });
    app.get('/single-post', (req, res, next) => {
-      res.render('single-post');
+      res.render('single-post',{
+         latestNews: latestNewsData
+      });
    });
 
 };
