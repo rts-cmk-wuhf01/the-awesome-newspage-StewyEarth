fa99ce0958ef5568a68784c0dcf1ad033ac437a7
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-06-04 10:22:14 +0200

    Popular news home foreach, single post comments foreach and time

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,27 +1,31 @@
 module.exports = (app) => {
    let popularNewsHomeData = [
       {
-         imgPath: "img/bg-img/7.jpg",
-         title: "Orci varius natoque penatibus et magnis",
-         postTime: "2019-04-14 06:00:14",
+         imgPath: "img/bg-img/12.jpg",
+         title: "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...",
+         likes: 21,
+         comments: 25,
+         postCategory: "Finance"
       },
       {
-         imgPath: "img/bg-img/8.jpg",
-         title: "Orci varius natoque penatibus et magnis",
-         postTime: "2019-04-13 07:00:14",
+         imgPath: "img/bg-img/13.jpg",
+         title: "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...",
+         likes: 2921,
+         comments: 255,
+         postCategory: "Finance"
       }, {
-         imgPath: "img/bg-img/9.jpg",
-         title: "Orci varius natoque penatibus et magnis",
-         postTime: "2019-04-12 08:00:14",
+         imgPath: "img/bg-img/14.jpg",
+         title: "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...",
+         likes: 15,
+         comments: 3,
+         postCategory: "Finance"
       }, {
-         imgPath: "img/bg-img/10.jpg",
-         title: "Orci varius natoque penatibus et magnis",
-         postTime: "2019-04-11 09:00:14",
-      }, {
-         imgPath: "img/bg-img/11.jpg",
-         title: "Orci varius natoque penatibus et magnis",
-         postTime: "2019-04-10 10:00:14",
-      }
+         imgPath: "img/bg-img/15.jpg",
+         title: "Dolor sit amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales placer. Sed varius leo ac...",
+         likes: 85,
+         comments: 13,
+         postCategory: "Finance"
+      },
    ]
    let worldNewsAllData = [
       {
@@ -135,7 +139,7 @@ module.exports = (app) => {
          commentedPost: "Facebook is offering facial recognition..."
       }
    ]
-   let popularNewsData = [
+   let mostPopularNewsData = [
       {
          title: "Amet, consectetur adipiscing elit. Nam eu metus sit amet odio sodales.",
          postTime: "2019-04-14 10:00:14",
@@ -153,14 +157,33 @@ module.exports = (app) => {
          postTime: "2015-07-31 10:00:14",
       }
    ]; 
-
+   let singlePostCommentsData = [
+      {
+         posterImgpath: "img/bg-img/30.jpg",
+         posterName: "Shanie Bolit",
+         postTime: "2019-06-04 10:00:14",
+         message: "Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed pellen tesque lectus et accu msan aliquam. Fusce lobortis cursus quam, id mattis sapien."
+      },
+      {
+         posterImgpath: "img/bg-img/31.jpg",
+         posterName: "Jamie Smith",
+         postTime: "2019-06-05 10:00:14",
+         message: "Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed pellen tesque lectus et accu msan aliquam. Fusce lobortis cursus quam, id mattis sapien."
+      },
+      {
+         posterImgpath: "img/bg-img/32.jpg",
+         posterName: "Igor Usio",
+         postTime: "2019-06-03 10:00:14",
+         message: "Donec turpis erat, scelerisque id euismod sit amet, fermentum vel dolor. Nulla facilisi. Sed pellen tesque lectus et accu msan aliquam. Fusce lobortis cursus quam, id mattis sapien."
+      },
+   ]
    app.get('/', (req, res, next) => {
       res.render('home', {
          latestNews: latestNewsData,
-         popularNews: popularNewsData,
+         mostPopularNews: mostPopularNewsData,
          editorsPicks: editorsPickData,
          worldNewsAll: worldNewsAllData,
-
+         popularNewsHomeArray: popularNewsHomeData,
       });
    });
    app.get('/about', (req, res, next) => {
@@ -170,7 +193,7 @@ module.exports = (app) => {
       res.render('catagories-post', {
          latestNews: latestNewsData,
          latestComments: latestCommentsData,
-         popularNews: popularNewsData,
+         mostPopularNews: mostPopularNewsData,
       });
    });
    app.get('/contact', (req, res, next) => {
@@ -180,7 +203,8 @@ module.exports = (app) => {
       res.render('single-post', {
          latestNews: latestNewsData,
          latestComments: latestCommentsData,
-         popularNews: popularNewsData,
+         mostPopularNews: mostPopularNewsData,
+         singlePostComments: singlePostCommentsData,
       });
    });
 
