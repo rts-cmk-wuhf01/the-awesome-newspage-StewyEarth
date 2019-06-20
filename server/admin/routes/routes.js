95c6d6744ddfa2e3420532a3e43031f4cf2d3c0a
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-06-20 13:10:16 +0200

    Added include for adminpanel nav. Dynamic user count.

--- server/admin/routes/routes.js
+++ server/admin/routes/routes.js
@@ -6,7 +6,16 @@ const sqlcalls = require("../../sql-services/calls")
 module.exports = (app) => {
     app.get('/admin', async (req, res, next) => {
         let db = await mysql.connect();
+        let [userCountData] = await db.execute("select count(id) as count from users")
+        console.log(userCountData)
         db.end();
-        res.render("adminpanel")  //Loader HTML og muligvis sender noget data med som 2. parameter
+        res.render("adminpanel",{
+            userCount: userCountData[0]
+        })  //Loader HTML og muligvis sender noget data med som 2. parameter
+    });
+    app.get('/admin/contactmessages', async (req, res, next) => {
+        let db = await mysql.connect();
+        db.end();
+        res.render("adminpanel-contactmessages")  //Loader HTML og muligvis sender noget data med som 2. parameter
     });
 }
