119ad13ac80db48804c8be279a975b71c05c1aa1
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-06-25 14:42:04 +0200

    Added category add functionality

--- server/admin/routes/routes.js
+++ server/admin/routes/routes.js
@@ -7,15 +7,59 @@ module.exports = (app) => {
     app.get('/admin', async (req, res, next) => {
         let db = await mysql.connect();
         let [userCountData] = await db.execute("select count(id) as count from users")
-        console.log(userCountData)
         db.end();
-        res.render("adminpanel",{
+        res.render("adminpanel", {
             userCount: userCountData[0]
         })  //Loader HTML og muligvis sender noget data med som 2. parameter
     });
+
+    app.get('/admin/categories', async (req, res, next) => {
+        let db = await mysql.connect();
+        let categoriesData = await sqlcalls.getCategoriesData();
+        db.end();
+        res.render("adminpanel-categories", {
+            categories: categoriesData,
+        })
+    });
+
+
+    app.post('/admin/categories', async (req, res, next) => {
+        let name = req.body.name;
+        let return_message = "";
+
+        if (typeof name == "undefined" || name == '') {
+            return_message = 'Enter a name';
+        } else if (name.length < 2) {
+            return_message = "Enter a name thats more than 2 or more characters";
+        }
+        if (return_message.length == 0) {
+            let db = await mysql.connect();
+            let result = await db.execute(`
+            INSERT INTO postcategories 
+            SET
+            name = ? `
+                , [name]);
+            db.end();
+
+            if (result[0].affectedRows > 0) {
+                return_message = 'Category added';
+            } else {
+                return_message = 'Category could not be added';
+            }
+
+        }
+        res.render("adminpanel-categories", {
+            return_message: return_message,
+            categories: await sqlcalls.getCategoriesData()
+        })
+    });
+
+
+
     app.get('/admin/contactmessages', async (req, res, next) => {
         let db = await mysql.connect();
         db.end();
         res.render("adminpanel-contactmessages")  //Loader HTML og muligvis sender noget data med som 2. parameter
     });
+
 }
